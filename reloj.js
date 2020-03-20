02870f1c54df50cfa94f7bd9922dce7abab739a2
ivgomez <iv.gomez@hotmail.com>
2020-03-19 19:55:39 -0500

    first commit

---
+++ reloj.js
@@ -0,0 +1,7 @@
+const manecillaHora = document.getElementById("hora");
+const manecillaMinutos = document.getElementById("minutos");
+const manecillaSegundos = document.getElementById("segundos");
+
+function setDate() {
+  const horaActual = new Date();
+}
